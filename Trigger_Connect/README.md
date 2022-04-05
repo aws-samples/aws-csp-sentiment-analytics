daa8960dd557488ea1fc7d83a199ba54877e7f7f
revol909 <87537929+revol909@users.noreply.github.com>
2022-04-05 11:54:49 +0100

    Add files via upload

---
+++ Trigger_Connect/README.md
@@ -0,0 +1,2 @@
+# The  Lambda application "Trigger_Connect" serves the following functions: 1. it is triggered by a message being available on a SQS queue, 2. it triggers an Amazon Connect Contact Flow.
+# ContactFlowId, InstanceId, SourcePhoneNumber must be updated with the Amazon Connect instance being created.
---
+++ Trigger_Connect/Trigger_Connect.py
@@ -0,0 +1,37 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT-0
+    
+import json
+import boto3
+
+# Define the client to interact with Amazon Connect
+client = boto3.client('connect')
+
+def lambda_handler(event, context):
+    
+    message=event['Records'][0]['body']
+    receiptHandle=event['Records'][0]['receiptHandle']
+    
+    l = message.split(',')
+    event_time=l[0]
+    sensorid=l[1]
+    event_type=l[2]
+    event_status=l[3]
+    failure_rate_1h=l[5]
+    cell=l[6]
+    failure_rate_6h=l[7]
+    IEFS_ID=l[8]
+
+    response = client.start_outbound_voice_contact(
+    DestinationPhoneNumber=str(sensorid),
+    # add your ContactFlowIDnumber
+    ContactFlowId=ContactFlowIDnumber,
+    # add your InstanceIDnumber
+    InstanceId=InstanceIDnumber,
+    SourcePhoneNumber='ConnectSourcePhoneNumber',
+    Attributes={
+        "cell":cell
+        }
+    )
+    
+    return response
